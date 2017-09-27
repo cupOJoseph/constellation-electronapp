1f3a9f89e615d57604cc11e9e29d2da48bf61459
jschiarizzi <jschiarizzi@gwu.edu>
2017-09-27 10:41:00 -0400

    add built applications.

---
+++ electron-app-linux-ia32/resources/app/README.md
@@ -0,0 +1,5 @@
+# constellation-electronapp
+
+This is a practice project. It is an example on how you can easily use electron to create desktop applications using only web technologies.
+
+More info at https://services.github.com/on-demand/electron/
---
+++ electron-app-mas-x64/electron-app.app/Contents/Resources/app/src/cs.js
@@ -0,0 +1,14 @@
+instances = new Array();
+constellationPreset('cs','Flying Stuff');
+
+/*
+Love is in the air:
+2,10,0.5,0.2,0.2,90,1.5,0.05,100,320,100,95,320,80,90,0.05,0,0.04,true
+
+elementID, starDensity 2, starRadius 10, starRadiusJitter 0.5, starVelocity .2, starVelocityJitter 0.2, connectionRadius 90, connectionWidth 1.5, connectionOpacity .05, revealRadius 100, backgroundHue 320,  backgroundSaturation 100, backgroundLightness 95, foregroundHue 320, foregroundSaturation 80, foregroundLightness 90, foregroundHueJitter .05, foregroundSaturationJitter 0, foregroundLightnessJitter .04, responsive
+
+DCD:
+"cs,"2,10,0.5,0.2,0.2 ,90, 1.5,0.05, 100,320,100,95,320,80,90,0.05,0,0.04,false
+*/
+
+/*Hue, saturation, lightness*/
